effa935db15cddecd3cabd9ccb9a335c79f2e608
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2018-06-23 21:00:16 +0600

    Added Compare argument for heap_sort and cleaned-up test file

--- heap_sort.hpp
+++ heap_sort.hpp
@@ -3,13 +3,15 @@
 
 #include <iterator>
 #include <algorithm>
+#include <functional>
 
 namespace shino
 {
-    template <typename RandomAccessIterator>
+    template <typename RandomAccessIterator, typename Compare = std::less<>>
     void push_heap(RandomAccessIterator first,
                    RandomAccessIterator last,
-                   RandomAccessIterator element)
+                   RandomAccessIterator element,
+                   Compare cmp = {})
     {
         auto dist = std::distance(first, element) + 1;
         auto left_child = first + dist * 2 - 1;
@@ -18,53 +20,53 @@ namespace shino
         auto right_child = first + dist * 2;
         if (right_child >= last)
         {
-            if (*element < *left_child)
+            if (cmp(*element, *left_child))
             {
                 std::iter_swap(element, left_child);
-                push_heap(first, last, left_child);
+                push_heap(first, last, left_child, cmp);
             }
             return;
         }
 
-        if (*element >= *left_child and *element >= *right_child)
+        if (not cmp(*element, *left_child) and not cmp (*element, *right_child))
             return;
 
         auto next_location =
-                (*left_child >= *right_child) ? left_child : right_child;
+                (not cmp(*left_child,*right_child)) ? left_child : right_child;
         std::iter_swap(next_location, element);
-        shino::push_heap(first, last, next_location);
+        shino::push_heap(first, last, next_location, cmp);
     }
 
-    template <typename RandomAccessIterator>
-    void build_heap(RandomAccessIterator first, RandomAccessIterator last)
+    template <typename RandomAccessIterator, typename Compare = std::less<>>
+    void build_heap(RandomAccessIterator first, RandomAccessIterator last, Compare cmp = {})
     {
         for (auto middle = first + std::distance(first, last) / 2;
              middle != first; --middle)
         {
-            shino::push_heap(first, last, middle);
+            shino::push_heap(first, last, middle, cmp);
         }
 
-        shino::push_heap(first, last, first);
+        shino::push_heap(first, last, first, cmp);
     }
 
-    template <typename RandomAccessIterator>
-    void pop_heap(RandomAccessIterator first, RandomAccessIterator last)
+    template <typename RandomAccessIterator, typename Compare = std::less<>>
+    void pop_heap(RandomAccessIterator first, RandomAccessIterator last, Compare cmp = {})
     {
         if (std::distance(first, last) < 2)
             return;
 
         auto new_last = std::prev(last);
         std::iter_swap(first, new_last);
-        shino::push_heap(first, new_last, first);
+        shino::push_heap(first, new_last, first, cmp);
     }
 
-    template <typename RandomAccessIterator>
-    void sort_heap(RandomAccessIterator first, RandomAccessIterator last)
+    template <typename RandomAccessIterator, typename Compare = std::less<>>
+    void sort_heap(RandomAccessIterator first, RandomAccessIterator last, Compare cmp = {})
     {
         auto current_last = last;
         while (first != current_last)
         {
-            shino::pop_heap(first, current_last);
+            shino::pop_heap(first, current_last, cmp);
             --current_last;
         }
     }
--- heap_sort_test.cpp
+++ heap_sort_test.cpp
@@ -1,8 +1,7 @@
-//#include "heap_sort.hpp"
+#include "heap_sort.hpp"
 #include <vector>
 #include <iostream>
 #include <random>
-#include <unordered_set>
 #include <numeric>
 #include <algorithm>
 
@@ -21,89 +20,11 @@ std::vector<int> generate_vector(std::size_t size)
     return numbers;
 }
 
-std::vector<int> global_v{generate_vector(4)};
-
-#include <iterator>
-
-namespace shino
-{
-    template <typename RandomAccessIterator>
-    void push_heap(RandomAccessIterator first,
-                   RandomAccessIterator last,
-                   RandomAccessIterator element)
-    {
-        auto dist = std::distance(first, element) + 1;
-        auto left_child = first + dist * 2 - 1;
-        if (left_child >= last)
-            return;
-        auto right_child = first + dist * 2;
-        if (right_child >= last)
-        {
-            if (*element < *left_child)
-            {
-                std::iter_swap(element, left_child);
-                push_heap(first, last, left_child);
-            }
-            return;
-        }
-
-        if (*element >= *left_child and *element >= *right_child)
-            return;
-
-        auto next_location =
-                (*left_child >= *right_child) ? left_child : right_child;
-        std::iter_swap(next_location, element);
-        shino::push_heap(first, last, next_location);
-    }
-
-    template <typename RandomAccessIterator>
-    void build_heap(RandomAccessIterator first, RandomAccessIterator last)
-    {
-        for (auto middle = first + std::distance(first, last) / 2;
-             middle != first; --middle)
-        {
-            shino::push_heap(first, last, middle);
-        }
-
-        shino::push_heap(first, last, first);
-    }
-
-    template <typename RandomAccessIterator>
-    void pop_heap(RandomAccessIterator first, RandomAccessIterator last)
-    {
-        if (std::distance(first, last) < 2)
-            return;
-
-        auto new_last = std::prev(last);
-        std::iter_swap(first, new_last);
-        shino::push_heap(first, new_last, first);
-    }
-
-    template <typename RandomAccessIterator>
-    void sort_heap(RandomAccessIterator first, RandomAccessIterator last)
-    {
-        auto current_last = last;
-        while (first != current_last)
-        {
-            shino::pop_heap(first, current_last);
-            --current_last;
-        }
-    }
-}
-
 #include <algorithm>
 
 int main()
 {
-//    shino::build_heap(global_v.begin(), global_v.end());
-//    if (not std::is_heap(global_v.begin(), global_v.end()))
-//        std::cerr << "not a heap\n";
-//    shino::sort_heap(global_v.begin(), global_v.end());
-//
-//    for (int x: global_v)
-//        std::cout << x << ' ';
-//    std::cout << '\n';
-    for (std::size_t i = 0; i <= 10'000; ++i)
+    for (std::size_t i = 0; i <= 30'000; ++i)
     {
         std::vector<int> v(generate_vector(i));
 
